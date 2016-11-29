4b4a5e9ff07477745050bc8227f3d257aa74c541
Rony Nogueira <ronynogueiras.dev@gmail.com>
2016-11-29 21:38:13 -0200

    remove old project

--- src/main/saep/ApplicationServiceLayerSaep.java
+++
@@ -1,8 +0,0 @@
-public interface ApplicationServiceLayerSaep {
-    void criarPonuacao();
-    List<String> obterConfiguracoes();
-    InputStream obterParecerPDF();
-    InputStream obterParecerHTML();
-    InputStream obterRelatorioPDF();
-    InputStream obterRelatorioHTML();
-}
