5f6eecde484d6f9686603a3aa74febbd96115ed5
Rony Nogueira <ronynogueiras.dev@gmail.com>
2016-12-01 22:06:45 -0200

    Adicionados novos tipos de contenttype

--- src/util/ContentType.java
+++ src/util/ContentType.java
@@ -3,4 +3,6 @@ package util;
 public interface ContentType {
 	String JSON = "application/json";
 	String PDF  = "application/pdf";
+	String HTML = "text/html";
+	String PLAIN_TEXT = "text/plain";
 }
