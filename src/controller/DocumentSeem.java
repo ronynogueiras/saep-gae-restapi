dcae13913bf4be34ba5a3ecbc947dc892d38a873
Rony Nogueira <ronynogueiras.dev@gmail.com>
2016-12-01 22:52:53 -0200

    implementação de métodos para o parecer close #4

--- src/controller/DocumentSeem.java
+++ src/controller/DocumentSeem.java
@@ -1,19 +1,142 @@
 package controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
+import util.ContentStatus;
+import util.ContentType;
+import util.ErrorCode;
+@SuppressWarnings("serial")
 public class DocumentSeem extends HttpServlet {
 
+	
+private ApplicationService application = new ApplicationService();
+	
 	/**
+	 * Recupera o relatório (RADOC) indicado
+	 * @author Rony Nogueira
 	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+	 * @see {http://docs.saep.apiary.io/#reference/0/saepparecerid/get}
+	 * @param HttpServletRequest
+	 * @param HttpServletResponse
+	 * @throws ServletException
+	 * @throws IOException
+	 * */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+		//No-used
+		String uuid = request.getPathInfo();
 		
+		String responseText = "";
+		String contentType;
+		int status;
+		
+		InputStream radocHTML = application.getDocumentHTML();
+		
+		if (radocHTML != null){
+			status = ContentStatus.OK;
+			contentType = ContentType.HTML;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
+			int reads = radocHTML.read(); 
+			while(reads != -1){ 
+				baos.write(reads); 
+				reads = radocHTML.read();
+			}
+			OutputStream output = response.getOutputStream();
+			output.write(baos.toByteArray());
+			output.close();
+			response.setContentLength(output.toString().length());
+		}else{
+			contentType = ContentType.JSON;
+			status = ContentStatus.SERVER_ERROR;
+			JSONObject json = new JSONObject();
+			try{
+				json.put("codigo", ErrorCode.Unknown.CODE);
+				json.put("mensagem", ErrorCode.Unknown.MESSAGE);
+				responseText = json.toString();
+			}catch(JSONException e){
+				contentType = ContentType.PLAIN_TEXT;
+				responseText = "erro inesperado";
+			}
+			response.setContentLength(responseText.length());
+			response.getWriter().write(responseText);
+			response.setContentLength(responseText.length());
+		}
+		response.setStatus(status);
+		response.setContentType(contentType);		
+	}
+	/**
+	 * Cria um parecer com o UUID fornecido
+	 * @author Rony Nogueira
+	 * @see {http://docs.saep.apiary.io/#reference/0/saepparecerid/post}
+	 * @param HttpServletRequest
+	 * @param HttpServletResponse
+	 * */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String report = request.getParameter("resolucao");
+		String uuid = request.getPathInfo();
+		boolean isCreate = application.createDocument(uuid,report);
+		String responseText = "";
+		int status;
+		if(isCreate){
+			status = ContentStatus.NO_CONTENT;
+		}else{
+			JSONObject json = new JSONObject();
+			try{
+				json.put("codigo", ErrorCode.Unknown.CODE);
+				json.put("mensagem", ErrorCode.Unknown.MESSAGE);
+				responseText = json.toString();
+				status = ContentStatus.SERVER_ERROR;
+			}catch(JSONException e){
+				status = ContentStatus.SERVER_ERROR;
+				responseText = "erro inesperado";
+			}
+		}
+		response.setStatus(status);
+		response.setContentType(ContentType.JSON);
+		response.setContentLength(responseText.length());
+		response.getWriter().write(responseText);
+	}
+	/**
+	 * Apaga um parecer através do UUID fornecido
+	 * @author Rony Nogueira
+	 * @see {http://docs.saep.apiary.io/#reference/0/saepparecerid/delete}
+	 * @param HttpServletRequest
+	 * @param HttpServletResponse
+	 * @throws ServletException
+	 * @throws IOException
+	 * */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String uuid = request.getPathInfo();
+		String responseText="";
+		int status;
+		boolean isDelete = application.deleteDocument(uuid);
+		if(isDelete){
+			status = ContentStatus.NO_CONTENT;
+		}else{
+			try{
+				status = ContentStatus.SERVER_ERROR;
+				JSONObject json = new JSONObject();
+				json.put("message", ErrorCode.Unknown.MESSAGE);
+				responseText = json.toString();
+			}catch(JSONException e){
+				status = ContentStatus.SERVER_ERROR;
+				responseText = ErrorCode.Unknown.MESSAGE;
+			}
+		}
+		response.setContentType(ContentType.JSON);
+		response.setStatus(status);
+		response.setContentLength(responseText.length());
+		response.getWriter().write(responseText);
 	}
 	
 }
